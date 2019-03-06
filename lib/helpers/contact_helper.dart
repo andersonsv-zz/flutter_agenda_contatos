0274a6fddd15186816961f319d6addd9292a98cd
ANDERSON S VIEIRA <anderson.vieira-ext@viavarejo.com.br>
2019-03-06 12:42:52 -0300

    Inclusao de floating e desenho da tela inicial

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -28,7 +28,7 @@ class ContactHelper {
 
   Future<Database> initDb() async {
     final databasesPath = await getDatabasesPath();
-    final path = join(databasesPath, "contacts.db");
+    final path = join(databasesPath, "contactsnew.db");
 
     return await openDatabase(path, version: 1,
         onCreate: (Database db, int newerVersion) async {
@@ -103,6 +103,9 @@ class Contact {
   String email;
   String phone;
   String img;
+
+
+  Contact();
 
   Contact.fromMap(Map map) {
     id = map[idColumn];
